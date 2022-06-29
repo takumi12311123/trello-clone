aa91cc7e2bcdac9a364d763516494e6b7f6156f9
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-06-29 15:15:21 +0900

    taskのdrag and dropを実装(未完成)

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -16,6 +16,7 @@ export const TaskAddInput = ({
       {
         id: taskList.length,
         text: inputText,
+        draggableId: `task-${taskList.length}`
       },
     ]);
     setInputText("");
