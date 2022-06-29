ec4f01ccd4596526a32dd59d3810da8331c41265
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-06-29 13:32:28 +0900

    component Task and Tasks 編集

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 
-export const TaskAddInput = () => {
-  const handleSubmit = (e) > {
-
-  }
+export const TaskAddInput = ({
+  inputText,
+  setInputText,
+  setTaskList,
+  taskList,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTaskList([
+      ...taskList,
+      {
+        text: inputText,
+      },
+    ]);
+    setInputText("");
+  };
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="add a task" className="taskAddInput"/>
+        <input
+          type="text"
+          placeholder="add a task"
+          className="taskAddInput"
+          onChange={handleChange}
+          value={inputText}
+        />
       </form>
     </div>
   );
